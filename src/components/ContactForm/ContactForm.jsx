17480080029432d2540f00061f1a79809963a581
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-06-03 17:47:34 +0300

    Update pattern in ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -37,7 +37,7 @@ export class ContactForm extends Component {
                         value={name}
                         onChange={this.handleChange}
                         placeholder='Enter name'
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
                     />
@@ -48,7 +48,7 @@ export class ContactForm extends Component {
                         value={number}
                         onChange={this.handleChange}
                         placeholder='Enter number'
-                        pattern="\+?\d+(\s|-|\d)*\d+"
+                        pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                     />
