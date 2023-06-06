add01c0ee72411fee8ede348d5bd16595b44cde5
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-06-06 12:17:48 +0300

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Component } from "react";
 import { Frame, Container, Title, SubTitle } from "./App.styled";
-import { ContactForm } from "../ContactForm";
-import { ContactList } from "../ContactList";
-import { Filter } from "../Filter";
+import { ContactForm } from "./components/ContactForm";
+import { ContactList } from "./components/ContactList";
+import { Filter } from "./components/Filter";
 import { nanoid } from "nanoid";
-import defaultContacts from '../../data/contacts.json'
+import defaultContacts from '../src/data/contacts.json'
 
 
 export class App extends Component {
